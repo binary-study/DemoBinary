4b336f6fb7b22b74a6c8ce45ddbeb2d6cc962783
binary-study <bianrystudy21@gmail.com>
2024-03-30 08:53:11 +0000

    First commit from the Colab using github token

--- myfile.py
+++ myfile.py
@@ -2,3 +2,4 @@ name = input("Enter Your good Name: ")
 print("your Name is ", name)
 print("Added 2nd print statement")
 print("i have added 4th line")
+# using github token we are pushing from the colab
