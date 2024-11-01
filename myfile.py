3b63458437ff4c1f42310682f925d40d7a95240c
Binary Study <79457525+binary-study@users.noreply.github.com>
2024-11-02 00:56:18 +0530

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,7 +1,2 @@
 name = input("Enter Your good Name: ")
 print("your Name is ", name)
-print("Added 2nd print statement")
-print("i have added 4th line")
-# using github token we are pushing from the colab on 14-07-24
-# This line is added from colab on seond of October 2024
-# gtgt
